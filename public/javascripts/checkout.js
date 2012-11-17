ed8b83051965f0787592c489ba6a660c7755802a
Cristina <cristina@conekta.mx>
2012-11-17 16:23:41 -0600

    mes

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,4 +1,26 @@
 $(function(){
+
+
+
+$(".item-row input.quantity").change(function() {
+
+
+
+var total = 0;
+$(".item-row input.quantity").bind("keyup", function(e){
+
+var quantity_value = $(this).attr("value");
+
+total = total + parseInt(quantity_value) * parseFloat($(this).next(".price").html());
+
+});
+
+$("#order-total").html(total)
+
+
+});
+
+
 });
 
 
