d83393330c847c6adea2662b1325b953fb057d73
Cristina <cristina@conekta.mx>
2012-11-27 00:25:40 -0600

    mes

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -72,8 +72,6 @@ var input_val = $(this).attr("value") * parseInt($(this).next(".price").html());
 //total = input_val + total;
 var selector_string = "#rebate_" + $(this).attr("id");
 var input_val = Math.round(input_val*.1*100)/100;
-alert(selector_string);
-alert(input_val);
 $(selector_string).html(input_val);
 total_rebate = input_val + total_rebate;
 });
