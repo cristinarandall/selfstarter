0cfc52137c2fca4b29e8d1ce26760d1a346357a7
Cristina <cristina@conekta.mx>
2012-11-27 00:36:40 -0600

    mes

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -170,6 +170,19 @@ calculate_discount();
 
 
 
+$(".ordertype-item").hover(function(){
+
+var image_url = $(this).attr("data-url");
+alert(image_url);
+$("img.preview-image").attr("src", image_url);
+
+});
+
+
+
+
+
+
 
 
 });
