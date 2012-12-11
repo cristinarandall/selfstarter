825ecf35aae1bc16c83a1cc256e4742da31fc788
Cristina <cristina@conekta.mx>
2012-12-10 22:39:13 -0600

    mes

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,6 +2,27 @@
 class Notifier < ActionMailer::Base
 
 
+def order_status(order, items,email)
+  @order = order
+  @items = items
+  @order_email = order.email
+  @deposit = order.deposit
+  @balance = order.balance
+  @total = order.total
+
+  m = mail(
+  :subject=>'Order Notification from Gritworks',
+  :to=>"cristinarandall@gmail.com",
+  :from => "payments@gritworks.com",
+  :content_type => "text/html"#,
+  ) do |format|
+    format.html
+  end
+
+  m.deliver
+end
+
+
 def company_message(message, name, telephone, email, company)
     @sender_email = email
     @message = message
