3a61ae22146a47857eae10ed38a17c7a51ea9a09
Cristina <cristina@conekta.mx>
2012-12-17 11:38:25 -0600

    mes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user
+
+
+  def current_user_session
+#    puts session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    @current_user = current_user_session && current_user_session.record
+  end
+
+
 end
