884a3f9005f1a3126da05b7c36c974a1702baece
Cristina <cristina@conekta.mx>
2012-12-17 09:27:03 -0600

    mes

--- db/migrate/20121204040318_change_id.rb
+++ db/migrate/20121204040318_change_id.rb
@@ -1,6 +1,6 @@
 class ChangeId < ActiveRecord::Migration
   def up
-change_column :orders, :balance_payment_transaction_id, :string
+add_column :orders, :balance_payment_transaction_id, :string
 
   end
 
