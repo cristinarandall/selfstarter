403e6fff41e035a38e77c32ed5eab91de4abd6fb
Cristina <cristina@conekta.mx>
2012-12-20 12:58:28 -0600

    Mes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :persistence_token, :password_salt, :password_salt, :perishable_token
+  attr_accessible :email, :name, :persistence_token, :password_salt, :password_salt, :perishable_token, :password, :active
   has_many :orders
 
 
