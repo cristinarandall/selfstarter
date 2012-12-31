ff296670ca9818999c9a4a6abe329d45334d5b8f
Cristina <cristina@conekta.mx>
2012-12-30 22:14:40 -0600

    mes

--- db/migrate/20121203155542_add_name.rb
+++ db/migrate/20121203155542_add_name.rb
@@ -1,8 +1,6 @@
 class AddName < ActiveRecord::Migration
   def up
 
-add_column :orders, :balance_paid, :boolean
-add_column :orders, :deposit_paid, :boolean
 add_column :orders, :deposit_payment_transaction_id, :string
   end
 
