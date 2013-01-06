715ed2e5b699edd3fcce4fff9b0c2d42730b65c0
Cristina <cristina@conekta.mx>
2013-01-06 14:38:54 -0600

    mes

--- db/migrate/20121225195108_country.rb
+++ db/migrate/20121225195108_country.rb
@@ -1,7 +1,10 @@
 class Country < ActiveRecord::Migration
   def up
 
-change_column :orders, :shipping_cost, :float
+
+
+remove_column :orders, :shipping_cost
+add_column :orders, :shipping_cost, :float
   end
 
   def down
